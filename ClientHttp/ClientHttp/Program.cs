a5bbfdba77e6ea7b3e48e8c9db87eaf8b76c3e35
gitvanya34 <gitvanya34@mail.ru>
2021-10-18 21:59:13 +0300

    +demo gif

--- ClientHttp/ClientHttp/Program.cs
+++ ClientHttp/ClientHttp/Program.cs
@@ -27,17 +27,42 @@ namespace ClientHttp
 
         static async Task Main()
         {
-            //for (int i=0;i<50;i++)
-            //await getKey("123");
-            //await postMessage();
+            //for (int i = 0; i < 50; i++)
+            //{
+            //   await  getKey(generateKey());
+            //   await postMessage(userName, userKey, "TestMessage");
+            //}
+            //Console.ReadKey();
         }
+        private static string generateKey()
+        {
 
+
+            // Создаем массив букв, которые мы будем использовать.
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            // Создаем генератор случайных чисел.
+            Random rand = new Random();
+
+            // Сделайте слово.
+            string word = "";
+            for (int j = 1; j <= 4; j++)
+            {
+                // Выбор случайного числа от 0 до 25
+                // для выбора буквы из массива букв.
+                int letter_num = rand.Next(0, letters.Length - 1);
+                Thread.Sleep(1);//контроль рандомной генерации
+                                // Добавить письмо.
+                word += letters[letter_num];
+            }
+            return word;
+        }
         public static async Task getKey(string name)
         {
           
             try
             {
-                Console.WriteLine("Введите имя пользователя:");
+                Console.WriteLine(name+": Запрос на получение ключа");
                 userName = name /*"test2"*//* Console.ReadLine()*/;
 
                 HttpResponseMessage response = await client.GetAsync("http://localhost:8000/getKey/"+userName);
